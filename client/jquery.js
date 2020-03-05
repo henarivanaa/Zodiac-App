418a572b45b9a5c4936b9bb5535f9a765c594c44
umbone <henarivanaagit@gmail.com>
2020-03-05 21:54:27 +0700

    1. adding birthdate input form in index.html 2. adding event handler (on submit) in jquery.js

---
+++ client/jquery.js
@@ -0,0 +1,25 @@
+let $birthdateForm = $('#birthdate-form')
+let $birthdateInput = $('#birthdate-input')
+let $birthmonthInput = $('#birthmonth-input')
+
+$birthdateForm.on('submit', (e) => {
+    e.preventDefault()
+    let birthdate = $birthdateInput.val()
+    let birthmonth = $birthmonthInput.val()
+    addBirthdate(birthdate, birthmonth)
+})
+
+const addBirthdate = (birthdate, birthmonth) => {
+    $.ajax({
+        method: 'post',
+        url: 'http://localhost:3000/zodiacs',
+        data: {
+            "birthdate" : birthdate,
+            "birthmonth" : birthmonth
+        },
+        success: () => {
+            console.log('success')
+            $birthdateForm[0].reset()
+        }
+    })
+}
