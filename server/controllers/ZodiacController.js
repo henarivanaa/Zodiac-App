e1eacc2aa79ca47e96ee794e00b50c5b5c269fb3
umbone <henarivanaagit@gmail.com>
2020-03-05 22:18:20 +0700

    test

--- server/controllers/ZodiacController.js
+++ server/controllers/ZodiacController.js
@@ -2,13 +2,20 @@ const axios = require('axios')
 
 class ZodiacController {
     static getZodiacData = (req, res, next) => {
+        let birthdate = req.body.birthdate
         axios.get('https://zodiacal.herokuapp.com/api')
             .then(response => {
-                res.status(200).json(response.data)
+                let zodiacs = response.data
+                zodiacs.forEach(zodiac => {
+                    let startDate = zodiac.sun_dates[0].split(' ')
+                    let endDate = zodiac.sun_dates[1].split(' ')
+                    
+                })
             })
             .catch(err => {
                 console.log(err, 'ini error')
             })
+        
     }
 }
 
--- server/routes/zodiacRoutes.js
+++ server/routes/zodiacRoutes.js
@@ -1,6 +1,15 @@
 const router = require('express').Router()
 const ZodiacController = require('../controllers/ZodiacController')
 
-router.get('/', ZodiacController.getZodiacData)
+router.post('/', ZodiacController.getZodiacData)
 
-module.exports = router
+module.exports = router<div class="form-group">
+    <label for="exampleFormControlSelect1">Example select</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
